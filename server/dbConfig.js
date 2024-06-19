a6a87a6a59ca9ab8ecb3d2b2eb482d030bd51aa7
Chaitanya lakra <119479117+sascke07@users.noreply.github.com>
2024-06-19 17:13:37 +0530

    Update dbConfig.js

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -2,9 +2,10 @@ require('dotenv').config()
 const mongoose = require("mongoose");
 
 const dbConfig = async () => {
+    const MONGO_URI="mongodb+srv://Munu1234:UiFOtllXepY93Plu@cluster0.g2vdz2w.mongodb.net/test";
     try {
 
-       await mongoose.connect(process.env.MONGO_URI, {
+       await mongoose.connect(MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
