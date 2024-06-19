bc257063756a87fba345a1340cc954afa3a529d4
Chaitanya lakra <119479117+sascke07@users.noreply.github.com>
2024-06-19 15:59:55 +0530

    Update dbConfig.js

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -2,15 +2,19 @@ require('dotenv').config()
 const mongoose = require("mongoose");
 
 const dbConfig = async () => {
- const url = `mongodb+srv://Munu1234:UiFOtllXepY93Plu@cluster0.g2vdz2w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-    try{
-        await mongoose.connect(url);
-        console.log("database connected")
+    try {
 
-    }
-    catch(error){
-        console.log(`error while connection to database` , error.message)
+       await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log("db connected successfully");
+
+    } catch (error) {
+        console.log("something went wrong with db connection", error);
+        return;
     }
 }
+
 
 module.exports = dbConfig
